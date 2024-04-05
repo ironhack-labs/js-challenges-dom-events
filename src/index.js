bf2008b701f3ccecde1b9b56a69bd9530d148996
JustAdiii <adriantloczek@gmail.com>
2024-04-05 15:15:54 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
 
-
 // ITERATION 0 | Example Row
 // Splice 1 element from the contacts array at the random index
 const randomIndex = Math.floor(Math.random() * contacts.length);
@@ -30,31 +29,57 @@ exampleRow.innerHTML = `
 
 tableBody.appendChild(exampleRow);
 
+const buttonDelete = exampleRow.querySelector(".btn-delete");
+buttonDelete.addEventListener("click", () => {
+  exampleRow.remove();
+});
+// ITERATION 3 - Like Buttons
 
-
-
+// Your code goes here ...
+const likedButton = exampleRow.querySelector(".btn-like");
+likedButton.addEventListener("click", () => {
+  likedButton.classList.toggle("selected");
+});
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
+threeContacts.map((contact) => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = ` <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
 
-
-  
+  tableBody.appendChild(newRow);
   // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
 
+  // Your code goes here ...
+
+  const buttonDelete = newRow.querySelector(".btn-delete");
+  buttonDelete.addEventListener("click", () => {
+    newRow.remove();
+  });
   // ITERATION 3 - Like Buttons
 
   // Your code goes here ...
-
-  
-  
-
+  const likedButton = newRow.querySelector(".btn-like");
+  likedButton.addEventListener("click", () => {
+    likedButton.classList.toggle("selected");
+  });
+});
 
 // Bonus: ITERATION 4 - Add Random Contacts
 
