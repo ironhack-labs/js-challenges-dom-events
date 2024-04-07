e0083b3ead7d24f092bad6960ed52e4527f08d9b
Mira Witte <mira.witte@gmx.de>
2024-04-07 20:34:30 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
 
-
 // ITERATION 0 | Example Row
 // Splice 1 element from the contacts array at the random index
 const randomIndex = Math.floor(Math.random() * contacts.length);
@@ -30,32 +29,50 @@ exampleRow.innerHTML = `
 
 tableBody.appendChild(exampleRow);
 
-
-
-
-
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
-const threeContacts = contacts.splice(0, 3);
+const threeContacts = contacts.slice(0, 3);
 
 // Your code goes here ...
+const firstContact = threeContacts[0];
 
+// Function to create a row for a contact
+function createRow(contact) {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+    <td>
+      <img src="${contact.pictureUrl}" />
+    </td>
+    <td>${contact.name}</td>
+    <td>${contact.popularity.toFixed(2)}</td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+  `;
 
-  
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
+  // Add event listener to delete button
+  const deleteButton = newRow.querySelector(".btn-delete");
+  deleteButton.addEventListener("click", function () {
+    newRow.remove();
+  });
 
-  // ITERATION 3 - Like Buttons
+  // Add event listener to like button
+  const likeButton = newRow.querySelector(".btn-like");
+  likeButton.addEventListener("click", function () {
+    likeButton.classList.toggle("selected");
+  });
 
-  // Your code goes here ...
+  return newRow;
+}
 
-  
-  
-
-
-// Bonus: ITERATION 4 - Add Random Contacts
-
-// Your code goes here ...
+// Iterate over the first 3 contacts and create rows for them
+const firstThreeContacts = contacts.slice(0, 3);
+firstThreeContacts.forEach((contact) => {
+  const newRow = createRow(contact);
+  tableBody.appendChild(newRow);
+});
