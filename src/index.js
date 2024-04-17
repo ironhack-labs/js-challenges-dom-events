5e5d6f1edba373ae8e6b6f819bb1be7e03b95be8
Yassine Moussaif <yassine.moussaif1234@gmail.com>
2024-04-17 19:57:01 +0200

    "domProject"

--- src/index.js
+++ src/index.js
@@ -36,19 +36,73 @@ tableBody.appendChild(exampleRow);
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
-const threeContacts = contacts.splice(0, 3);
+// Iteration 1 - Display 3 contacts
+const threeContacts = contacts.splice(0, 3); 
 
-// Your code goes here ...
+threeContacts.forEach(contact => {
+  let tr = document.createElement('tr'); // Create a new row for each contact
+
+
+  let tdPicture = document.createElement('td');
+  tdPicture.innerHTML = `<img src="${contact.pictureUrl}" style="height:100px;">`; // Adjust the size as necessary
+  tr.appendChild(tdPicture);
+
+  
+  let tdName = document.createElement('td');
+  tdName.textContent = contact.name;
+  tr.appendChild(tdName);
+
+  
+  let tdPopularity = document.createElement('td');
+  tdPopularity.textContent = contact.popularity.toFixed(2);
+  tr.appendChild(tdPopularity);
+
+ 
+  let tdDelete = document.createElement('td');
+  tdDelete.innerHTML = `<button class="btn-delete">Delete</button>`;
+  tr.appendChild(tdDelete);
+  const deleteButton = tdDelete.querySelector(".btn-delete");
+  console.log(deleteButton);
+  deleteButton.addEventListener('click', function() {
+    
+    this.parentNode.parentNode.remove();
+  });
+
+  
+  let tdLike = document.createElement('td');
+  tdLike.innerHTML = `<button class="btn-like"><img src="./images/icon.png" alt="like"/></button>`;
+  tr.appendChild(tdLike);
+
+  const favoriteButton = tdLike.querySelector(".btn-like");
+  console.log(favoriteButton);
+  favoriteButton.addEventListener('click', function() {
+    this.classList.toggle('selected');
+  });
+
+  
+
+  
+  let tbody = document.querySelector("tbody#contacts");
+  tbody.appendChild(tr);
+});
 
 
   
   // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
+  const deleteButton = document.querySelector(".btn-delete");
+  console.log(deleteButton);
+  deleteButton.addEventListener('click', function() {
+    
+    this.parentNode.parentNode.remove();
+  });
 
   // ITERATION 3 - Like Buttons
+
+  const favoriteButton = document.querySelector(".btn-like");
+  console.log(favoriteButton);
+  favoriteButton.addEventListener('click', function() {
+    this.classList.toggle('selected');
+  });
 
   // Your code goes here ...
 
@@ -59,3 +113,12 @@ const threeContacts = contacts.splice(0, 3);
 // Bonus: ITERATION 4 - Add Random Contacts
 
 // Your code goes here ...
+
+
+const choices = document.querySelectorAll('input[name="choice"]');
+console.log(choices);
+choices.forEach((choice) => {
+  if(choice.checked) {
+    selectedAnswer = choice.value;
+  }
+});
