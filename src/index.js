24358fef1797b17b58dd4773600c5f2fce0c5f7b
Águeda Muela <agmuelamar@gmail.com>
2024-04-17 15:39:33 +0200

    lab contacts terminado

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
 
-
 // ITERATION 0 | Example Row
 // Splice 1 element from the contacts array at the random index
 const randomIndex = Math.floor(Math.random() * contacts.length);
@@ -11,8 +10,8 @@ const splicedArr = contacts.splice(randomIndex, 1);
 // Get the element from the spliced array
 const randomContact = splicedArr[0];
 
-const exampleRow = document.createElement("tr");
-exampleRow.innerHTML = `
+const rowNode = document.createElement("tr");
+rowNode.innerHTML = `
   <td>
     <img src="${randomContact.pictureUrl}" />
   </td>
@@ -28,33 +27,55 @@ exampleRow.innerHTML = `
   </td>
 `;
 
-tableBody.appendChild(exampleRow);
+tableBody.appendChild(rowNode);
+const deleteBtnNode = rowNode.querySelector(".btn-delete");
+deleteBtnNode.addEventListener("click", () => {
+  deleteBtnNode.remove();
+});
 
-
-
+const likeBtnNode = rowNode.querySelector(".btn-like");
+  likeBtnNode.addEventListener("click", () => {
+    likeBtnNode.classList.toggle("selected");
+});
 
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
+console.log(threeContacts);
 
-// Your code goes here ...
+const eachRow = threeContacts.forEach((eachContact) => {
+  //para cada elemento añadimos un tr, es decir, una fila
+  const rowNode = document.createElement("tr");
+  //dentro de cada fila, añadimos un contacto:
+  rowNode.innerHTML = `<td>
+    <img src="${eachContact.pictureUrl}" />
+  </td>
+  <td> ${eachContact.name} </td>
+  <td> ${eachContact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+  `;
+  tableBody.appendChild(rowNode);
 
-
-  
   // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
+  const deleteBtnNode = rowNode.querySelector(".btn-delete");
+  deleteBtnNode.addEventListener("click", () => {
+    deleteBtnNode.remove();
+  });
 
   // ITERATION 3 - Like Buttons
-
-  // Your code goes here ...
-
-  
-  
-
+  const likeBtnNode = rowNode.querySelector(".btn-like");
+  likeBtnNode.addEventListener("click", () => {
+    likeBtnNode.classList.toggle("selected");
+  });
+});
 
 // Bonus: ITERATION 4 - Add Random Contacts
 
