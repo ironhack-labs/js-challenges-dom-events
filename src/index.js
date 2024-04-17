0a49631e9bc63a9a4da14e9b4358df64566e5270
magdakorgul <magdalenakorgul1@gmail.com>
2024-04-17 22:52:52 +0200

    solved lab

--- src/index.js
+++ src/index.js
@@ -27,18 +27,68 @@ exampleRow.innerHTML = `
     </button>
   </td>
 `;
+const buttonToDelete = exampleRow.querySelector(".btn-delete");
+
+buttonToDelete.addEventListener("click", () =>{
+  exampleRow.remove();
+});
+
+const likedButton = exampleRow.querySelector(".btn-like");
+
+likedButton.addEventListener("click", () =>{
+  likedButton.classList.toggle("selected");
+});
 
 tableBody.appendChild(exampleRow);
-
-
-
-
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
+console.log(threeContacts);
 
-// Your code goes here ...
+threeContacts.forEach(contact => {
+  const newRow = document.createElement("tr");
+
+
+  newRow.innerHTML = `
+  <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>`
+
+
+const buttonToDelete = newRow.querySelector(".btn-delete");
+
+buttonToDelete.addEventListener("click", () =>{
+  newRow.remove();
+});
+
+
+
+
+const likedButton = newRow.querySelector(".btn-like");
+
+likedButton.addEventListener("click", () =>{
+  likedButton.classList.toggle("selected");
+});
+
+  
+
+
+    tableBody.appendChild(newRow);
+
+});
+
+
 
 
   
