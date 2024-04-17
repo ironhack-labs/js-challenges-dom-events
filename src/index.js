23c729096f78eaab1362dd285ce0f3609321a0e0
“Erno <ernohilarion@MacBook-Air-de-Erno.local>
2024-04-17 20:21:03 +0200

    remove button imposible

--- src/index.js
+++ src/index.js
@@ -31,21 +31,49 @@ exampleRow.innerHTML = `
 tableBody.appendChild(exampleRow);
 
 
-
-
-
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
 
-// Your code goes here ...
+threeContacts.forEach(contact => {
+  const newRow = document.createElement("tr");
+
+  newRow.innerHTML = `
+        <td><img src="${contact.pictureUrl}" /></td>
+        <td>${contact.name}</td>
+        <td>${contact.popularity.toFixed(2)}</td>
+        <td><button class="btn-delete">Delete</button></td>
+        <td><button class="btn-like"><img src="./images/icon.png" alt="like" /></button></td>
+    `;
+
+  tableBody.appendChild(newRow);
+});
 
 
-  
   // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
+
+// const deleteButton = threeContacts.querySelector(`.btn-delete`);
+// const clickButton = deleteButton.removeEventListener(`click`, () => {
+//   threeContacts.remove(); 
+// };
+
+
+let deleteButton = newRow.querySelector('.btn-delete');
+deleteButton.addEventListener('click', () => {
+
+  newRow.remove();
+});
+
+
+
+
+
+
+
+
+
+
+
   
 
   // ITERATION 3 - Like Buttons
