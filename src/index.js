c3925f44e616216088c8255511db0991e40e90a3
Asier MD <asier.md09@gmail.com>
2024-04-17 14:44:09 +0200

    done

--- src/index.js
+++ src/index.js
@@ -36,26 +36,88 @@ tableBody.appendChild(exampleRow);
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
-const threeContacts = contacts.splice(0, 3);
+const threeContacts = contacts.splice(0, 3)
 
 // Your code goes here ...
+const rngIndex = Math.floor(Math.random() * contacts.length)
+const rngConArr = contacts.splice(rngIndex, 3)
+console.log(rngIndex)
+console.log(rngConArr)
 
+rngConArr.forEach((element, index) => {
+  let newRow = document.createElement("tr")
+  newRow.innerHTML = `
+  <td>
+    <img src="${rngConArr[index].pictureUrl}" />
+  </td>
+  <td> ${rngConArr[index].name} </td>
+  <td> ${rngConArr[index].popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>`
 
-  
   // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
-
+  let btnDelete = newRow.querySelector(".btn-delete")
+  console.log(btnDelete)
+  btnDelete.addEventListener("click", deleteRow)
+    function deleteRow(){
+      console.log('boton clickado')
+      newRow.remove()
+    }
   // ITERATION 3 - Like Buttons
-
-  // Your code goes here ...
-
-  
-  
-
+  let btnLike = newRow.querySelector(".btn-like")
+  console.log(btnLike)
+  btnLike.addEventListener("click", like)
+    function like(){
+      console.log(btnLike)
+      console.log('boton like clickado')
+      btnLike.classList.toggle("selected")
+    }
+   
+  tableBody.appendChild(newRow)//crea la nueva fila
+});
 
 // Bonus: ITERATION 4 - Add Random Contacts
-
-// Your code goes here ...
+buttonAddRandom.addEventListener("click", addRandom)
+function addRandom() {
+const aleatorio = Math.floor(Math.random() * contacts.length)
+const addRow = document.createElement("tr");
+addRow.innerHTML = `
+  <td>
+    <img src="${contacts[aleatorio].pictureUrl}" />
+  </td>
+  <td> ${contacts[aleatorio].name} </td>
+  <td> ${contacts[aleatorio].popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+// ITERATION 2 - Delete Buttons
+let btnDelete = addRow.querySelector(".btn-delete")
+console.log(btnDelete)
+btnDelete.addEventListener("click", deleteRow)
+  function deleteRow(){
+    console.log('boton clickado')
+    addRow.remove()
+  }
+// ITERATION 3 - Like Buttons
+let btnLike = addRow.querySelector(".btn-like")
+console.log(btnLike)
+btnLike.addEventListener("click", like)
+  function like(){
+    console.log(btnLike)
+    console.log('boton like clickado')
+    btnLike.classList.toggle("selected")
+  }
+tableBody.appendChild(addRow);
+}
