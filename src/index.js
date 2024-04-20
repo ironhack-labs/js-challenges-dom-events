9a6a07d5ecf26afe9db36b773a1777b02d69db96
Ronnie Vélez <ronnievelz@gmail.com>
2024-04-20 12:03:42 +0200

    lab done

--- src/index.js
+++ src/index.js
@@ -39,23 +39,78 @@ tableBody.appendChild(exampleRow);
 const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
+threeContacts.forEach((contact)=>{
+const contactNode=document.createElement("tr");
+contactNode.innerHTML=
+`
+  <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+tableBody.appendChild(contactNode);
+console.log(contactNode);
 
+// ITERATION 2 - Delete Buttons
+  
+const deleteButton=contactNode.querySelector(".btn-delete")
+deleteButton.addEventListener("click", ()=>{
+  contactNode.remove()
+})
 
-  
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
+// ITERATION 3 - Like Buttons
 
-  // ITERATION 3 - Like Buttons
+const likeButton = contactNode.querySelector(".btn-like")
+likeButton.addEventListener("click", ()=>{
+  likeButton.classList.toggle("selected");
+});
 
-  // Your code goes here ...
-
-  
-  
-
+});
 
 // Bonus: ITERATION 4 - Add Random Contacts
 
 // Your code goes here ...
+
+buttonAddRandom.addEventListener("click",()=>{
+  
+  const splicedArr2=contacts.splice(randomIndex,1);
+  const randomContact2=splicedArr2[0]
+  const randomContactNode=document.createElement("tr")
+  randomContactNode.innerHTML=
+  `
+  <td>
+    <img src="${randomContact2.pictureUrl}" />
+  </td>
+  <td> ${randomContact2.name} </td>
+  <td> ${randomContact2.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+tableBody.appendChild(randomContactNode)
+const deleteButton=randomContactNode.querySelector(".btn-delete")
+deleteButton.addEventListener("click", ()=>{
+  randomContactNode.remove()
+})
+
+// ITERATION 3 - Like Buttons
+
+const likeButton = randomContactNode.querySelector(".btn-like")
+likeButton.addEventListener("click", ()=>{
+  likeButton.classList.toggle("selected");
+});
+})
