8efe96767bc9d2c4510029b75f59a92dde8bf9fe
Guillaume Duramé <durameg@gmail.com>
2024-05-05 14:22:24 +0200

    done

--- src/index.js
+++ src/index.js
@@ -39,19 +39,55 @@ tableBody.appendChild(exampleRow);
 const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
-
-
-  
+threeContacts.forEach((contacts) =>{
+  const newLigne = document.createElement('tr');
+  newLigne.innerHTML =`
+  <td>
+    <img src="${contacts.pictureUrl}"/>
+  </td>
+  <td>
+    ${contacts.name}
+  </td>
+  <td>
+    ${contacts.popularity.toFixed(2)}  
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>`;
+ 
   // ITERATION 2 - Delete Buttons
-  
+  // Syntaxe querySelector => element = document.querySelector(selecteurs);
+
   // Your code goes here ...
-  
-  
+
+  const deleteButton = newLigne.querySelector(".btn-delete")
+  deleteButton.addEventListener("click", (e) => {
+    e.target.closest("tr").remove();
+    //newLigne.remove();
+  })
 
   // ITERATION 3 - Like Buttons
 
   // Your code goes here ...
 
+  const likeButton = newLigne.querySelector(".btn-like");
+  likeButton.addEventListener("click", (e) => {
+    likeButton.classList.toggle("selected");
+  })
+
+
+
+  tableBody.appendChild(newLigne)
+});
+
+
+  
+
+  
   
   
 
