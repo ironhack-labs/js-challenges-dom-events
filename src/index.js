633c3a05202be3b69110f619438cf55fa9984a68
DaniDias95 <guiguimu_art@hotmail.com>
2024-05-15 17:37:21 +0100

    done

--- src/index.js
+++ src/index.js
@@ -31,12 +31,29 @@ exampleRow.innerHTML = `
 tableBody.appendChild(exampleRow);
 
 
-
-
-
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
+//Created a forEach method to iterate over the 3 contacts array
+threeContacts.forEach(contact => {
+  const threeRowAdd = document.createElement("tr");
+  threeRowAdd.innerHTML = `
+<td>
+<img src="${contact.pictureUrl}" />
+</td>
+<td> ${contact.name} </td>
+<td> ${contact.popularity.toFixed(2)} </td>
+<td>
+<button class="btn-delete">Delete</button>
+</td>
+<td>
+<button class="btn-like">
+  <img src="./images/icon.png" alt="like" />
+</button>
+</td>
+`
+tableBody.appendChild(threeRowAdd);
+});
 
 // Your code goes here ...
 
@@ -45,17 +62,61 @@ const threeContacts = contacts.splice(0, 3);
   // ITERATION 2 - Delete Buttons
   
   // Your code goes here ...
+function addDeleteFunction (row){
+  row.querySelector(".btn-delete").addEventListener("click", () => {
+    console.log("deleted");
+    //remove is a method that removes that element, so call the element and use .remove()
+    row.remove();
+  })
+  
+}
+tableBody.querySelectorAll(`tr`).forEach((row)=>{
+  addDeleteFunction(row);
+ })
   
   
 
   // ITERATION 3 - Like Buttons
 
   // Your code goes here ...
-
-  
-  
+  function toggleLikeBtn (row){
+    row.querySelector(".btn-like").addEventListener("click", ()=>{
+      console.log("toggle clicked");
+      row.querySelector(".btn-like").classList.toggle("selected")
+    })
+  }
+  tableBody.querySelectorAll(`tr`).forEach((row)=>{
+    toggleLikeBtn(row);
+   })
 
 
 // Bonus: ITERATION 4 - Add Random Contacts
 
 // Your code goes here ...
+
+buttonAddRandom.addEventListener("click", ()=>{
+  const randContactNew = contacts.splice((Math.floor(Math.random() * contacts.length)), 1)[0];
+// Creating a new row of contacts
+  const randomRow = document.createElement("tr");
+  randomRow.innerHTML = `
+  <td>
+    <img src="${randContactNew.pictureUrl}" />
+  </td>
+  <td> ${randContactNew.name} </td>
+  <td> ${randContactNew.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+
+//Called the function to delete and toggle like
+addDeleteFunction(randomRow);
+toggleLikeBtn(randomRow);
+
+tableBody.appendChild(randomRow);
+})
