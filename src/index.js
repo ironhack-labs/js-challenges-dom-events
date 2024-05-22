ee76b3e271662bf0261418fb72f5a036ee975805
Reda Dahmane <r.dahmanee@gmail.com>
2024-05-22 18:36:54 +0200

    Can't make the buttons work...

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
 
-
 // ITERATION 0 | Example Row
 // Splice 1 element from the contacts array at the random index
 const randomIndex = Math.floor(Math.random() * contacts.length);
@@ -10,7 +9,6 @@ const splicedArr = contacts.splice(randomIndex, 1);
 
 // Get the element from the spliced array
 const randomContact = splicedArr[0];
-
 const exampleRow = document.createElement("tr");
 exampleRow.innerHTML = `
   <td>
@@ -30,32 +28,63 @@ exampleRow.innerHTML = `
 
 tableBody.appendChild(exampleRow);
 
-
-
-
-
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
-const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
 
+const splicedArray = contacts.splice(0, 3);
+splicedArray.forEach((contact) => {
+  const row = document.createElement("tr");
+  row.innerHTML = `
+  <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
 
-  
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
+  tableBody.appendChild(row);
+});
 
-  // ITERATION 3 - Like Buttons
+const newRow = document.createwElement("tr");
+newRow.classList.add("tr");
 
-  // Your code goes here ...
+const buttonCell = document.createAttribute("td");
 
-  
-  
+const removeButton = newRow.querySelectorAll("#contact button");
+removeButton.textContent = "Remove";
+removeButton.addEventListener("click", function () {
+  newRow.remove();
+});
+buttonCell.appendChild(removeButton);
+newRow.appendChild(buttonCell);
+tableBody.appendChild(newRow);
 
+// Your code goes here ...
 
+// ITERATION 3 - Like Buttons
+
+// Your code goes here ...
+function createLikeRow() {
+  var newLikeRow = document.createElement("tr");
+  var likeButton = document.createElement("button");
+  likeButton.textContent = "Like";
+  likeButton.className = "btn-like";
+  likeButton.addEventListener("click", function () {
+    newLikeRow.classList.toggle("selected");
+  });
+  newLikeRow.appendChild(likeButton);
+  table.appendChild(newLikeRow);
+}
 // Bonus: ITERATION 4 - Add Random Contacts
 
 // Your code goes here ...
