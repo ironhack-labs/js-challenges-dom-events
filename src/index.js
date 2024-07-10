0385fba2efdddf1be350ffabb6f8115a962dcc72
Erickhub32 <erick.arevalolopez25@gmail.com>
2024-07-10 18:55:47 +0200

    Solved

--- src/index.js
+++ src/index.js
@@ -30,13 +30,51 @@ exampleRow.innerHTML = `
 
 tableBody.appendChild(exampleRow);
 
+/*
+Get the first 3 contacts from the contacts array.
+You can use the splice() method for this.Remember that the splice() method modifies the original array, and returns the results as a new array.
+Iterate over the newly obtained array of 3 contacts and, for each contact, create a new table row and append it to the table body.
+You can use the Iteration 0 code as an example of how to create a new table row and what its structure should be.
+Append each new table row to the table body.You can use the appendChild() method for this.
 
+ const buttonTags = document.querySelectorAll('button')
+        const titleTag = document.querySelector('h1')
 
+        buttonTags.forEach(eachButton => {
 
-
-// ITERATION 1 - Display 3 contacts
+            eachButton.onclick = () => {
+                titleTag.style.color = 'red'
+            }
+*/
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
+
+
+threeContacts.forEach((eachContact) => {
+  const tableContacs = document.createElement("tr");
+  
+  tableContacs.innerHTML = `
+  <td>
+    <img src="${eachContact.pictureUrl}" />
+  </td>
+  <td> ${eachContact.name} </td>
+  <td> ${eachContact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`
+
+  tableBody.appendChild(tableContacs);
+})
+
+//})
+
+
 
 // Your code goes here ...
 
