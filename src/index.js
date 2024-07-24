1581b6dca55f5ea14a465944201403b4fc171430
Casperavr <casper@hhc.earth>
2024-07-24 16:27:17 +0200

    done

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const splicedArr = contacts.splice(randomIndex, 1);
 // Get the element from the spliced array
 const randomContact = splicedArr[0];
 
+
 const exampleRow = document.createElement("tr");
 exampleRow.innerHTML = `
   <td>
@@ -28,7 +29,7 @@ exampleRow.innerHTML = `
   </td>
 `;
 
-tableBody.appendChild(exampleRow);
+  tableBody.appendChild(exampleRow);
 
 
 
@@ -40,12 +41,95 @@ const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
 
+threeContacts.forEach((contactObj) => {
+
+  const newRow = document.createElement("tr");
+
+  newRow.innerHTML = `
+  <td>
+    <img src="${contactObj.pictureUrl}" />
+  </td>
+  <td> ${contactObj.name} </td>
+  <td> ${contactObj.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>`;
+
+  tableBody.appendChild(newRow);
+  
+
+  // add delete button functionality
+  let delButton = newRow.querySelector(".btn-delete");
+  delButton.addEventListener("click", () => {newRow.remove()});
+
+
+  // add like button functionality
+  let likeButton = newRow.querySelector(".btn-like");
+  likeButton.addEventListener("click", () => {likeButton.classList.toggle("selected")})
+
+})
 
   
+
+
+
+buttonAddRandom.addEventListener("click", () => {
+  const newRow = document.createElement("tr");
+
+  const newRandomIndex = Math.floor(Math.random() * contacts.length);
+  const newSplicedArr = contacts.splice(newRandomIndex, 1);
+  const newRandomContact = newSplicedArr[0];
+
+
+
+  newRow.innerHTML = `
+  <td>
+    <img src="${newRandomContact.pictureUrl}" />
+  </td>
+  <td> ${newRandomContact.name} </td>
+  <td> ${newRandomContact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>`;
+
+  tableBody.appendChild(newRow);
+  
+
+  // add delete button functionality
+  let delButton = newRow.querySelector(".btn-delete");
+  delButton.addEventListener("click", () => {newRow.remove()});
+
+
+  // add like button functionality
+  let likeButton = newRow.querySelector(".btn-like");
+  likeButton.addEventListener("click", () => {likeButton.classList.toggle("selected")})
+});
+
+
+
+
+
+
+
+
+
   // ITERATION 2 - Delete Buttons
   
   // Your code goes here ...
   
+
+
+
   
 
   // ITERATION 3 - Like Buttons
