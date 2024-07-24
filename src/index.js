3d203646beb50ac35bea1f21828741a201489fd2
Amrata MOHAMED <amrata.m.mohamed@gmail.com>
2024-07-24 22:39:32 +0200

    done

--- src/index.js
+++ src/index.js
@@ -32,21 +32,51 @@ tableBody.appendChild(exampleRow);
 
 
 
-
-
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
 
-// Your code goes here ...
+// Your code goes here ... 
+console.log(threeContacts); 
 
+threeContacts.forEach(contact => {
+  console.log(contact);
 
+const otherContacts = threeContacts[0];
+
+  let row = document.createElement('tr'); 
+  row.innerHTML = `
+  <td>
+    <img src="${otherContacts.pictureUrl}" />
+  </td>
+  <td> ${otherContacts.name} </td>
+  <td> ${otherContacts.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+
+  let cell = document.createElement('td'); 
   
+  cell.otherContacts = contact;
+  row.appendChild(cell);
+  tableBody.appendChild(row);
+
+});
+
+
   // ITERATION 2 - Delete Buttons
   
   // Your code goes here ...
   
   
+  row.querySelector('#contacts');
+
 
   // ITERATION 3 - Like Buttons
 
