d6c87c140f577120b61d0657064499acf3ccda30
Favas Muhammed <favassakkeer13@gmail.com>
2024-09-04 18:12:37 +0200

    forth commit

--- src/index.js
+++ src/index.js
@@ -31,12 +31,33 @@ exampleRow.innerHTML = `
 tableBody.appendChild(exampleRow);
 
 
+const deleteButton = exampleRow.querySelector(".btn-delete");
 
+  deleteButton.addEventListener("click", () => {
+    exampleRow.remove();
+  });
+  
+  
+
+  // ITERATION 3 - Like Buttons
+
+  const likeButton = exampleRow.querySelector(".btn-like");
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("selected");
+  });
 
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.slice(0, 3);
+
+
+
+
+
+
+
 
 threeContacts.forEach(contact => {
   const newRow = document.createElement("tr");
@@ -66,12 +87,53 @@ threeContacts.forEach(contact => {
   const likeButton = newRow.querySelector(".btn-like");
 
   likeButton.addEventListener("click", () => {
-    newRow.classList.toggle("selected");
+    likeButton.classList.toggle("selected");
   });
   
   
 });
 
-// Bonus: ITERATION 4 - Add Random Contacts
+/* Bonus: ITERATION 4 - Add Random Contacts
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
 
-// Your code goes here ...
+
+const row = document.createElement('tr');
+row.innerHTML = `
+  <td>
+    <img src="${randomContact.pictureUrl}" />
+  </td>
+  <td>${randomContact.name}</td>
+  <td>${randomContact.popularity.toFixed(2)}</td>
+  <td><button class="btn-delete">Delete</button></td>
+  <td>
+  <button class="btn-like">
+    <img src="./images/icon.png" alt="like" />
+  </button>
+</td>
+`;
+
+const buttonDelete = row.querySelector('.btn-delete');
+buttonDelete.addEventListener('click', function () {
+  row.remove();
+});
+
+const buttonLike = row.querySelector('.btn-like');
+buttonLike.addEventListener('click', function () {
+  buttonLike.classList.toggle('selected');
+});
+
+tableBody.appendChild(row);
+});
+*/ 
