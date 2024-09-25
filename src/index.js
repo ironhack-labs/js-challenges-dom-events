9e5823fa165b76eee88340c6e5a0a4480cef6e61
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-09-25 17:38:30 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
 
-const randomIndex = Math.floor(Math.random() * contacts.length);
-const splicedArr = contacts.splice(randomIndex, 1);
-
-const randomContact = splicedArr[0];
-
-const likeButton = (button) =>
-  (button.onclick = () => button.classList.toggle("selected"));
-
-const deleteButton = (button, row) => (button.onclick = () => row.remove());
+const getRandomContact = () => {
+  const randomIndex = Math.floor(Math.random() * contacts.length);
+  const splicedArr = contacts.splice(randomIndex, 1);
+  const randomContact = splicedArr[0];
+  return randomContact;
+};
 
 const makeRow = (contact) => {
   const row = document.createElement("tr");
@@ -28,30 +24,22 @@ const makeRow = (contact) => {
       </button>
     </td>
   `;
+  tableBody.appendChild(row);
   const btnLike = row.querySelector(".btn-like");
+  btnLike.onclick = () => btnLike.classList.toggle("selected");
   const btnDelete = row.querySelector(".btn-delete");
-  likeButton(btnLike);
-  deleteButton(btnDelete, row);
+  btnDelete.onclick = () => row.remove();
   return row;
 };
 
+const randomContact = getRandomContact();
 const exampleRow = makeRow(randomContact);
-tableBody.appendChild(exampleRow);
 
 const threeContacts = contacts.splice(0, 3);
-
 const threeRows = threeContacts.map((contact) => makeRow(contact));
 
-tableBody.append(...threeRows);
-
 document.querySelector("#btn-add-random").onclick = () => {
-  const randomIndex = Math.floor(Math.random() * contacts.length);
-  const splicedArr = contacts.splice(randomIndex, 1);
-  const randomContact = splicedArr[0];
+  const randomContact = getRandomContact();
   const row = makeRow(randomContact);
-  const btnLike = row.querySelector(".btn-like");
-  const btnDelete = row.querySelector(".btn-delete");
-  likeButton(btnLike);
-  deleteButton(btnDelete, row);
-  tableBody.appendChild(row);
+  return row;
 };
