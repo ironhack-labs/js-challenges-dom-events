dc3ef7540a91cd63d25a85149b599d2d287df232
Daniel Emig <schattenpalkia@googlemail.com>
2024-09-25 16:10:15 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -2,36 +2,39 @@
 const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
 
-
 // ITERATION 0 | Example Row
 // Splice 1 element from the contacts array at the random index
-const randomIndex = Math.floor(Math.random() * contacts.length);
-const splicedArr = contacts.splice(randomIndex, 1);
 
-// Get the element from the spliced array
-const randomContact = splicedArr[0];
+function createContactRow(contact) {
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>
+      <img src="${contact.pictureUrl}" />
+    </td>
+    <td> ${contact.name} </td>
+    <td> ${contact.popularity.toFixed(2)} </td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+  `;
+  tableBody.appendChild(row);
 
-const exampleRow = document.createElement("tr");
-exampleRow.innerHTML = `
-  <td>
-    <img src="${randomContact.pictureUrl}" />
-  </td>
-  <td> ${randomContact.name} </td>
-  <td> ${randomContact.popularity.toFixed(2)} </td>
-  <td>
-    <button class="btn-delete">Delete</button>
-  </td>
-  <td>
-    <button class="btn-like">
-      <img src="./images/icon.png" alt="like" />
-    </button>
-  </td>
-`;
+  const delBtn = row.querySelector("button.btn-delete");
+  delBtn.addEventListener("click", () => 
+    row.remove()
+);
 
-tableBody.appendChild(exampleRow);
-
-
-
+  const likeBtn = row.querySelector("button.btn-like");
+  likeBtn.addEventListener("click", () =>
+     likeBtn.classList.toggle("selected")
+);
+  return row;
+}
 
 
 // ITERATION 1 - Display 3 contacts
@@ -39,8 +42,9 @@ tableBody.appendChild(exampleRow);
 const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
-
-
+threeContacts.forEach(element => {
+    createContactRow(element)
+});
   
   // ITERATION 2 - Delete Buttons
   
@@ -59,3 +63,6 @@ const threeContacts = contacts.splice(0, 3);
 // Bonus: ITERATION 4 - Add Random Contacts
 
 // Your code goes here ...
+buttonAddRandom.addEventListener("click", () =>
+    createContactRow(contacts[Math.floor(Math.random() * contacts.length)])
+);
