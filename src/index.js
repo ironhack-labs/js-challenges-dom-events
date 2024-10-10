f7810db1c67fdae79d872efe72170a398e45607e
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-10-10 17:14:39 +0200

    iteration 2 solved

--- src/index.js
+++ src/index.js
@@ -40,6 +40,31 @@ const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
 
+/* threeContacts.forEach(contact => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+  <td>
+  <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+  <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+  <button class="btn-like">
+  <img src="./images/icon.png" alt="like" />
+  </button>
+  </td>
+  `;
+  tableBody.appendChild(newRow);
+  }); 
+*/
+
+// ITERATION 2 - Delete Buttons
+
+// Your code goes here ...
+
 threeContacts.forEach(contact => {
   const newRow = document.createElement("tr");
   newRow.innerHTML = `
@@ -57,18 +82,17 @@ threeContacts.forEach(contact => {
   </button>
   </td>
   `;
-  tableBody.appendChild(newRow)
+  let deleteButton = newRow.querySelector('.btn-delete');
+  /* deleteButton.addEventListener('click', () => tableBody.removeChild(newRow)) */
+  deleteButton.addEventListener('click', () => newRow.remove())
+  tableBody.appendChild(newRow);
 });
   
-// ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
   
 
-  // ITERATION 3 - Like Buttons
+// ITERATION 3 - Like Buttons
 
-  // Your code goes here ...
+// Your code goes here ...
 
   
   
