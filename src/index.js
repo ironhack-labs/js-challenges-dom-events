d79a156d209f7b82355265b1ecf2f870594c34b9
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-10-10 17:55:59 +0200

    bonus iteration 4 solved

--- src/index.js
+++ src/index.js
@@ -120,10 +120,40 @@ threeContacts.forEach(contact => {
   tableBody.appendChild(newRow);
 });
 
-  
-  
-
 
 // Bonus: ITERATION 4 - Add Random Contacts
+
+// Your code goes here ...
+
+buttonAddRandom.addEventListener('click', () => {
+  const currentRandomNumber = Math.floor(Math.random() * contacts.length);
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+  <td>
+  <img src="${contacts[currentRandomNumber].pictureUrl}" />
+  </td>
+  <td> ${contacts[currentRandomNumber].name} </td>
+  <td> ${contacts[currentRandomNumber].popularity.toFixed(2)} </td>
+  <td>
+  <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+  <button class="btn-like">
+  <img src="./images/icon.png" alt="like" />
+  </button>
+  </td>
+  `;
+  let deleteButton = newRow.querySelector('.btn-delete');
+  /* deleteButton.addEventListener('click', () => tableBody.removeChild(newRow)) */
+  deleteButton.addEventListener('click', () => newRow.remove())
+  
+  let likeButton = newRow.querySelector('.btn-like');
+  likeButton.addEventListener('click', () => likeButton.classList.toggle('selected'))
+  
+  tableBody.appendChild(newRow);
+})
+
+
+// Bonus: ITERATION 5 - Sort Contacts by Name
 
 // Your code goes here ...
