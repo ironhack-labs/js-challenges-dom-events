e82bf0297f05fb103641365dded08ca02fe9679e
Andrea Bronzoni <bronzoni.andrea@libero.it>
2024-10-14 02:51:58 +0200

    Iteration 4 completed - lab completed

--- src/index.js
+++ src/index.js
@@ -40,8 +40,8 @@ const threeContacts = contacts.splice(0, 3);
 const firstContacts = threeContacts;
 console.log('Array with three contacts: ', firstContacts);
 firstContacts.forEach(elem => {
-  const exampleRow = document.createElement("tr");
-  exampleRow.innerHTML =`
+  const newRow = document.createElement("tr");
+  newRow.innerHTML =`
   <td> <img src="${elem.pictureUrl}" />
   </td><td> ${elem.name} </td>
   <td> ${elem.popularity.toFixed(2)} </td>
@@ -53,31 +53,67 @@ firstContacts.forEach(elem => {
     </button>
     </td>
   `;
-  tableBody.appendChild(exampleRow);
+  tableBody.appendChild(newRow);
 
   // ITERATION 2 - Delete Buttons
-  const button = exampleRow.querySelector(".btn-delete");
+  const button = newRow.querySelector(".btn-delete");
   button.addEventListener("click", () => {
     console.log ("button delete clicked");
-    exampleRow.remove(tableBody);
+    newRow.remove(tableBody);
 
   });
 
   // ITERATION 3 - Like Buttons
-const likeButton = exampleRow.querySelector (".btn-like");
+const likeButton = newRow.querySelector (".btn-like");
 likeButton.addEventListener("click", () => {
   console.log ("button like clicked");
-  const toggleElement = exampleRow.querySelector(".btn-like")
+  const toggleElement = newRow.querySelector(".btn-like")
   if (toggleElement.classList.contains("selected")) {  //classList takes the class within "."
       toggleElement.classList.remove("selected");
 } else {
   toggleElement.classList.add("selected");}
 });
 }); //cierra linea 42
-  
-  
 
 
 // Bonus: ITERATION 4 - Add Random Contacts
+const randomButton = document.querySelector("#btn-add-random");
+randomButton.addEventListener("click", () => {
+const randomIndex = Math.floor(Math.random()*contacts.length);
+const randomContact = (contacts.splice(randomIndex,1))[0];
+console.log("Add button clicked");
+console.log(randomContact);
+const newRow = document.createElement("tr");
+newRow.innerHTML =`
+  <td> <img src="${randomContact.pictureUrl}" />
+  </td><td> ${randomContact.name} </td>
+  <td> ${randomContact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td> 
+    <button class="btn-like"><img src="./images/icon.png" alt="like" />
+    </button>
+    </td>
+  `;
+  tableBody.appendChild(newRow);
 
-// Your code goes here ...
+  const button = newRow.querySelector(".btn-delete");
+  button.addEventListener("click", () => {
+    console.log ("button delete clicked");
+    newRow.remove(tableBody);
+   });
+
+   const likeButton = newRow.querySelector (".btn-like");
+   likeButton.addEventListener("click", () => {
+     console.log ("button like clicked");
+     const toggleElement = newRow.querySelector(".btn-like")
+     if (toggleElement.classList.contains("selected")) {  //classList takes the class within "."
+         toggleElement.classList.remove("selected");
+   } else {
+     toggleElement.classList.add("selected");}
+   });
+
+
+}) //closes line 81
+
