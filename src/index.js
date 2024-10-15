e25057aa903ba49baf1f0f1b584148f8063cf2f6
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-10-15 20:07:02 +0200

    iteration 5 beta version, it works somehow but has some bugs...

--- src/index.js
+++ src/index.js
@@ -158,57 +158,79 @@ buttonAddRandom.addEventListener('click', () => {
 
 // Your code goes here ...
 
+// 1. Create a new button with the text 'Sort by Name'.
 const body = document.querySelector('body');
 const sortButton = document.createElement('button');
 sortButton.innerHTML = 'Sort by Name';
 body.appendChild(sortButton);
 
+// 2. Create a new array with the contacts for the table. The array should contain all the contacts to be displayed in the table.
+
+
+
+/* 3. Create a helper function `showContacts()` that will display the contacts in the table. This function should take an array of contacts as an argument and display them in the table. When called the function should:
+    - Remove the existing rows from the table.
+    - Iterate over the array of contacts and, for each contact, create a new table row and append it to the table body.
+    - Add event listeners to the *Delete* and *Like* buttons of the new rows. 
+*/
+
+const showContacts = (contactsArr) => {    
+  // - Remove the existing rows from the table.
+  tableBody.innerHTML = '';
+
+  // - Iterate over the array of contacts and, for each contact, create a new table row and append it to the table body.
+  for (let i = 0; i < contactsArr.length; i++) {
+    const newRow = document.createElement("tr");
+    newRow.innerHTML = `
+    <td>
+    <img src="${contactsArr[i].contactImage}" />
+    </td>
+    <td> ${contactsArr[i].contactName} </td>
+    <td> ${contactsArr[i].popularityScore} </td>
+    <td>
+    <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+    <button class="btn-like">
+    <img src="./images/icon.png" alt="like" />
+    </button>
+    </td>
+    `;
+
+    tableBody.appendChild(newRow);
+    
+    // - Add event listeners to the *Delete* and *Like* buttons of the new rows.
+    let deleteButton = newRow.querySelector('.btn-delete');
+    deleteButton.addEventListener('click', () => newRow.remove())
+    
+    let likeButton = newRow.querySelector('.btn-like');
+    likeButton.addEventListener('click', () => likeButton.classList.toggle('selected'))
+    
+  }
+}
 
 sortButton.addEventListener('click', () => {
-  let contactList = tableBody.getElementsByTagName('tr');
-  const arrayContactList = [...contactList];
+  const arrayContactList = [];
+  const myContacts = tableBody.querySelectorAll('tr');
+  myContacts.forEach(contact => {
+    const contactData = contact.querySelectorAll('td');
+    const contactObject = {
+      contactImage: contactData[0].getElementsByTagName('img')[0].src,
+      contactName: contactData[1].textContent,
+      popularityScore: contactData[2].textContent,
+    };
+    arrayContactList.push(contactObject);
+  })
+  console.log(arrayContactList);
+  
   arrayContactList.sort((a, b) => {
-    if (a.name > b.name) {
+    if (a.contactName > b.contactName) {
       return 1;
+    } else if (a.contactName === b.contactName) {
+      return 0;
     } else {
       return -1
     }
   });
-  const showContacts = (contactsArr) => {    
-
-    tableBody.innerHTML = '';
-
-    for (let i = 0; i < contactsArr.length; i++) {
-      const newRow = document.createElement("tr");
-      newRow.innerHTML = `
-      <td>
-      <img src="${contactsArr[i].pictureUrl}" />
-      </td>
-      <td> ${contactsArr[i].name} </td>
-      <td> ${contactsArr[i].popularity.toFixed(2)} </td>
-      <td>
-      <button class="btn-delete">Delete</button>
-      </td>
-      <td>
-      <button class="btn-like">
-      <img src="./images/icon.png" alt="like" />
-      </button>
-      </td>
-      `;
-      let deleteButton = newRow.querySelector('.btn-delete');
-      /* deleteButton.addEventListener('click', () => tableBody.removeChild(newRow)) */
-      deleteButton.addEventListener('click', () => newRow.remove())
-      
-      let likeButton = newRow.querySelector('.btn-like');
-      likeButton.addEventListener('click', () => likeButton.classList.toggle('selected'))
-      
-      tableBody.appendChild(newRow);
-    }
-  }
-
   showContacts(arrayContactList)
-
-
 })
-
-
