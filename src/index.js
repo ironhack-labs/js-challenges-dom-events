d751465ae766f8c55559da363d8f8d0d28d5581c
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-10-17 19:49:00 +0200

    fixing sort button (again) - work in progress

--- src/index.js
+++ src/index.js
@@ -50,9 +50,9 @@ const generateNewRow = (newContact) => {
 // ITERATION 1 - Display X contacts
 // Crete a function to get the first X contacts from the 'contacts' array.
 
-const getFirstContacts = (numberOfContacts) => {
-  const contactsArr = contacts.splice(0, numberOfContacts);
-  contactsArr.forEach(contact => {
+const getContacts = (numberOfContacts, contactsArray) => {
+  const newContactsArr = contactsArray.splice(0, numberOfContacts);
+  newContactsArr.forEach(contact => {
     const newContact = new Contact(contact);
     generateNewRow(newContact);
   })
@@ -87,65 +87,18 @@ sortButton.innerHTML = "Sort by Name";
 sortButton.setAttribute('style', 'margin: 0 28vw 0 6vw')
 buttonDivElement.insertBefore(sortButton, buttonAddRandom);
 
-// 2. Create a new array with the contacts for the table. The array should contain all the contacts to be displayed in the table.
-
-/* 3. Create a helper function `showContacts()` that will display the contacts in the table. This function should take an array of contacts as an argument and display them in the table. When called the function should:
-    - Remove the existing rows from the table.
-    - Iterate over the array of contacts and, for each contact, create a new table row and append it to the table body.
-    - Add event listeners to the *Delete* and *Like* buttons of the new rows. 
-*/
-
 const showContacts = (contactsArr) => {
   // - Remove the existing rows from the table.
-  tableBody.innerHTML = "";
+  tableBody.innerHTML = " ";
 
   // - Iterate over the array of contacts and, for each contact, create a new table row and append it to the table body.
-  for (let i = 0; i < contactsArr.length; i++) {
-    const newRow = document.createElement("tr");
-    newRow.innerHTML = `
-    <td>
-    <img src="${contactsArr[i].pictureUrl}" />
-    </td>
-    <td> ${contactsArr[i].name} </td>
-    <td> ${contactsArr[i].popularity} </td>
-    <td>
-    <button class="btn-delete">Delete</button>
-    </td>
-    <td>
-    <button class="btn-like">
-    <img src="./images/icon.png" alt="like" />
-    </button>
-    </td>
-    `;
-
-    tableBody.appendChild(newRow);
-
-    // - Add event listeners to the *Delete* and *Like* buttons of the new rows.
-    let deleteButton = newRow.querySelector(".btn-delete");
-    deleteButton.addEventListener("click", () => newRow.remove());
-
-    let likeButton = newRow.querySelector(".btn-like");
-    likeButton.addEventListener("click", () =>
-      likeButton.classList.toggle("selected")
-    );
-  }
+  getContacts(contactsArr.length, contactsArr);
 };
 
-sortButton.addEventListener("click", () => {
-  /*  const arrayContactList = [];
-  const myContacts = tableBody.querySelectorAll('tr');
-  myContacts.forEach(contact => {
-    const contactData = contact.querySelectorAll('td');
-    const contactObject = {
-      contactImage: contactData[0].getElementsByTagName('img')[0].src,
-      contactName: contactData[1].textContent,
-      popularityScore: contactData[2].textContent,
-    };
-    arrayContactList.push(contactObject);
-  })
-  console.log(allContacts); */
 
-  allContacts.sort((a, b) => {
+sortButton.addEventListener("click", () => {
+  console.log(Contact.allContacts)
+  Contact.allContacts.sort((a, b) => {
     if (a.name > b.name) {
       return 1;
     } else if (a.name === b.name) {
@@ -154,9 +107,10 @@ sortButton.addEventListener("click", () => {
       return -1;
     }
   });
-  console.log("después del sort", allContacts);
 
-  showContacts(allContacts);
+  console.log("después del sort", Contact.allContacts);
+
+  showContacts(Contact.allContacts);
 });
 
 
