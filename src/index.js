24ad27adde307a3c3d27be1cc8da610606ff5aae
Ana Molina <ana713w@gmail.com>
2024-10-19 17:56:18 +0200

    done

--- src/index.js
+++ src/index.js
@@ -40,22 +40,83 @@ const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
 
+threeContacts.forEach(contact => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+  <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>`;
 
-  
+  tableBody.appendChild(newRow);
+
   // ITERATION 2 - Delete Buttons
   
-  // Your code goes here ...
-  
-  
+  const deleteButton = newRow.querySelector(".btn-delete");
+
+  deleteButton.addEventListener("click", () => {
+    newRow.remove();
+  })
 
   // ITERATION 3 - Like Buttons
 
-  // Your code goes here ...
+  const likeButton = newRow.querySelector(".btn-like");
 
-  
-  
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("selected");
+  })
 
+})
 
 // Bonus: ITERATION 4 - Add Random Contacts
 
-// Your code goes here ...
+const randomContactButton = document.querySelector("#btn-add-random");
+
+randomContactButton.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * contacts.length);
+  const splicedArr = contacts.splice(randomIndex, 1);
+
+  const randomNewContact = splicedArr[0];
+
+  const newRandomRow = document.createElement("tr");
+  newRandomRow.innerHTML = `
+  <td>
+    <img src="${randomNewContact.pictureUrl}" />
+  </td>
+  <td> ${randomNewContact.name} </td>
+  <td> ${randomNewContact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>`;
+
+  tableBody.appendChild(newRandomRow);
+
+  
+  const deleteButton = newRandomRow.querySelector(".btn-delete");
+
+  deleteButton.addEventListener("click", () => {
+    newRandomRow.remove();
+  })
+
+  const likeButton = newRandomRow.querySelector(".btn-like");
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("selected");
+  })
+
+})
+
