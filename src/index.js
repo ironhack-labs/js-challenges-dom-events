7f885881be9145dac3e53a530cd1c3c36672c20d
Francesco Torchia <francescotorchia10@gmail.com>
2024-11-16 09:07:56 +0100

    myLab

--- src/index.js
+++ src/index.js
@@ -37,20 +37,61 @@ tableBody.appendChild(exampleRow);
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
+threeContacts.forEach(contact => {
+  const newRow = document.createElement('tr')
+  newRow.innerHTML = 
+  `
+  <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+tableBody.appendChild(newRow);
+});
+  const deleteBtn = newRow.querySelector(".btn-delete");
+  deleteBtn.addEventListener("click", () => {
+    newRow.remove();
+  });
+  const likeBtn = newRow.querySelector(".btn-like");
+  likeBtn.addEventListener("click", () => {
+  
+    likeBtn.classList.toggle("liked");
+    likeBtn.querySelector("img").src = likeBtn.classList.contains("liked")
+      ? "images/icon.png"
+      : "./images/icon.png";
+  });
+
 
 // Your code goes here ...
 
 
   
   // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
+  const deleteElement = document.querySelectorAll('.btn-delete')
+  deleteElement.forEach(button => {
+    button.addEventListener('click', function(){
+      button.closest('tr').remove();
+    });
+  });
   
   
 
   // ITERATION 3 - Like Buttons
-
-  // Your code goes here ...
+const likedButtons = document.querySelectorAll('.btn-like')
+  likedButtons.forEach(button => { 
+    button.addEventListener('click', function(){
+     button.classList.toggle('selected');
+    })
+  });
 
   
   
