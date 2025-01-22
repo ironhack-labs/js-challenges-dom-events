6afe9320772030d8c261e0a64f0d8b0e4007ee8d
Patricia Gracia Gutierrez <pgg.uxui@gmail.com>
2025-01-22 23:45:52 +0000

    done

--- src/index.js
+++ src/index.js
@@ -36,21 +36,43 @@ tableBody.appendChild(exampleRow);
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
+
 const threeContacts = contacts.splice(0, 3);
-
-// Your code goes here ...
-
-
+threeContacts.forEach(contact => {
+  const newRow = document.createElement("tr");
   
+  newRow.innerHTML = `
+      <td>
+        <img src="${contact.pictureUrl}" />
+      </td>
+      <td>${contact.name}</td>
+      <td>${contact.popularity}</td>
+      <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+  `;
+  tableBody.appendChild(newRow);
   // ITERATION 2 - Delete Buttons
   
-  // Your code goes here ...
-  
-  
+  const deleteButton=newRow.querySelector(".btn-delete");
+  deleteButton.addEventListener("click", () => {
+    newRow.remove();
+  });
+
 
   // ITERATION 3 - Like Buttons
+  newRow.querySelector(".btn-like").addEventListener("click", () => {
+    newRow.querySelector(".btn-like").classList.toggle("selected");
+  });
+  
+});
 
-  // Your code goes here ...
+  
 
   
   
