c66032143381b40de74623a0799aedcd97c71718
Lucas Shahbazi <lucasshahbazi@yahoo.de>
2025-01-22 15:45:45 +0100

    implemented inserting three random contacts, functionality for like and delete button

--- src/index.js
+++ src/index.js
@@ -38,19 +38,47 @@ tableBody.appendChild(exampleRow);
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
 
-// Your code goes here ...
+threeContacts.forEach(contact => {
+  const newRow = document.createElement("tr");
+newRow.innerHTML = `
+  <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+
+tableBody.appendChild(newRow);
+
+// Iteration 2
+const deleteButton = newRow.querySelector(".btn-delete");
+deleteButton.addEventListener("click", () => {
+  newRow.remove();
+  });
 
 
-  
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
+// ITERATION 3 - Like Buttons
 
-  // ITERATION 3 - Like Buttons
+const likeButton = newRow.querySelector(".btn-like");
+likeButton.addEventListener("click", () => {
+  likeButton.classList.toggle("selected");
+  });
 
-  // Your code goes here ...
+
+
+
+});
+
+
+
 
   
   
