9ec773ff7204904009ffe1240d46281e5df1149d
aoge2716 <aoge2716@gmail.com>
2025-01-22 17:10:33 +0100

    completed tasks and added a new listener that listens to whole table instead of just button

--- src/index.js
+++ src/index.js
@@ -10,52 +10,82 @@ const splicedArr = contacts.splice(randomIndex, 1);
 
 // Get the element from the spliced array
 const randomContact = splicedArr[0];
-
-const exampleRow = document.createElement("tr");
-exampleRow.innerHTML = `
-  <td>
-    <img src="${randomContact.pictureUrl}" />
-  </td>
-  <td> ${randomContact.name} </td>
-  <td> ${randomContact.popularity.toFixed(2)} </td>
-  <td>
-    <button class="btn-delete">Delete</button>
-  </td>
-  <td>
-    <button class="btn-like">
-      <img src="./images/icon.png" alt="like" />
-    </button>
-  </td>
-`;
-
-tableBody.appendChild(exampleRow);
+addRow(randomContact);
 
 
+function addRow(contact){
+  const exampleRow = document.createElement("tr");
+  exampleRow.innerHTML = `
+    <td>
+      <img src="${contact.pictureUrl}" />
+    </td>
+    <td> ${contact.name} </td>
+    <td> ${contact.popularity.toFixed(2)} </td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+  `;
+  
+  tableBody.appendChild(exampleRow);
+}
 
+function removeTable(index){
 
+}
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
+threeContacts.forEach(el => addRow(el))
 
-// Your code goes here ...
+
+// ITERATION 2 - Delete Buttons
+const delBtns = document.querySelectorAll('.btn-delete');
+// delBtns.forEach((el, i)=>{
+//   el.addEventListener("click",()=>{
+//     console.log(i);
+//     tableBody.deleteRow(i);
+//   })
+// })
+
+// delBtns.forEach((btn) => btn.addEventListener("click", (e)=>  e.target.closest("tr").remove()))
 
 
-  
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
+// // ITERATION 3 - Like Buttons
+// const likeBtns = document.querySelectorAll(".btn-like");
+// likeBtns.forEach(el=>{
+//   el.addEventListener("click",()=>{
+//     console.log("liked");
+//     el.classList.toggle("selected")
+//   })
+// })  
 
-  // ITERATION 3 - Like Buttons
+// constantly listening from the table
+tableBody.addEventListener("click", event =>{
+  // check if the closest thing to target is ".btn-delete" 
+  if (event.target.closest(".btn-delete")){
+    // remove the closest row 
+    event.target.closest("tr").remove();
+    console.log("removed");
+  // if closest classt is ".btn-like"
+  }else if (event.target.closest(".btn-like")){
+    // toggle "selected" on closest ".btn-like"
+    event.target.closest(".btn-like").classList.toggle("selected");
+    console.log("liked");
+  }
+});
 
-  // Your code goes here ...
-
-  
-  
 
 
 // Bonus: ITERATION 4 - Add Random Contacts
-
-// Your code goes here ...
+const rndmBtn = document.getElementById("btn-add-random")
+rndmBtn.addEventListener("click",()=>{
+  console.log("add random")
+  const randI = Math.floor(Math.random()* contacts.length);
+  addRow(contacts[randI])
+})
