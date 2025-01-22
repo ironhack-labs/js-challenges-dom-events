04ec699cd27bd17ba7939a35e7db5437e604f244
Robert Muresan <robotzelu89@gmail.com>
2025-01-22 15:45:09 +0000

    done

--- src/index.js
+++ src/index.js
@@ -36,26 +36,98 @@ tableBody.appendChild(exampleRow);
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
+
 const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
-
+threeContacts.forEach((contact) => {
+  const newContact = document.createElement("tr");
+  newContact.innerHTML = `
+    <td>
+      <img src="${contact.pictureUrl}" />
+    </td>
+    <td> ${contact.name} </td>
+    <td> ${contact.popularity.toFixed(2)} </td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+  `;
+  tableBody.appendChild(newContact);
+}
+);
 
   
   // ITERATION 2 - Delete Buttons
   
   // Your code goes here ...
   
+  const deleteButtons = document.querySelectorAll(".btn-delete");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.target.parentElement.parentElement.remove();
+    })
+  })
   
 
   // ITERATION 3 - Like Buttons
 
   // Your code goes here ...
 
+  const likeButtons = document.querySelectorAll(".btn-like");
+  likeButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      button.classList.toggle("selected");
+    })
+  })
   
   
 
 
 // Bonus: ITERATION 4 - Add Random Contacts
-
 // Your code goes here ...
+
+const addRandomContact = document.querySelector("#btn-add-random");
+
+
+addRandomContact.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * contacts.length);
+  const splicedArr = contacts.splice(randomIndex, 1);
+  const randomContact = splicedArr[0];
+
+  const ranContact = document.createElement("tr");
+  ranContact.innerHTML = `
+    <td>
+      <img src="${randomContact.pictureUrl}" />
+    </td>
+    <td> ${randomContact.name} </td>
+    <td> ${randomContact.popularity.toFixed(2)} </td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+  `;
+  tableBody.appendChild(ranContact);
+
+  const deleteButtons = document.querySelectorAll(".btn-delete");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.target.parentElement.parentElement.remove();
+    })
+  })
+
+  const likeButtons = document.querySelectorAll(".btn-like");
+  likeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      button.classList.toggle("selected");
+    })
+  })
+})
