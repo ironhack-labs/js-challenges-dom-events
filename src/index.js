c9f8291b13c01c4646c823f5abe95d84023087b1
Telmo Lousada <telmo.lousada@gmail.com>
2025-01-23 06:28:13 +0000

    Done

--- src/index.js
+++ src/index.js
@@ -10,16 +10,18 @@ const splicedArr = contacts.splice(randomIndex, 1);
 
 // Get the element from the spliced array
 const randomContact = splicedArr[0];
+addRow(randomContact);
 
-const exampleRow = document.createElement("tr");
-exampleRow.innerHTML = `
+function addRow(contact) {
+  const exampleRow = document.createElement("tr");
+  exampleRow.innerHTML = `
   <td>
-    <img src="${randomContact.pictureUrl}" />
+    <img src="${contact.pictureUrl}" />
   </td>
-  <td> ${randomContact.name} </td>
-  <td> ${randomContact.popularity.toFixed(2)} </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
   <td>
-    <button class="btn-delete">Delete</button>
+    <button class="btn-delete" data-index="${contacts.indexOf(contact)}">Delete</button>
   </td>
   <td>
     <button class="btn-like">
@@ -28,32 +30,34 @@ exampleRow.innerHTML = `
   </td>
 `;
 
-tableBody.appendChild(exampleRow);
-
-
+  tableBody.appendChild(exampleRow);
+}
 
 
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
+threeContacts.forEach(el => addRow(el))
 
-// Your code goes here ...
 
+// ITERATION 2 - Delete Buttons
+const delBtns = document.querySelectorAll('.btn-delete');
 
-  
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
+tableBody.addEventListener("click", event => {
+  // check if the closest thing to target is ".btn-delete" 
+  if (event.target.closest(".btn-delete")) {
+    // remove the closest row 
+    event.target.closest("tr").remove();
+    console.log("removed");
+    // if closest classt is ".btn-like"
+  } else if (event.target.closest(".btn-like")) {
+    // toggle "selected" on closest ".btn-like"
+    event.target.closest(".btn-like").classList.toggle("selected");
+    console.log("liked");
+  }
+});
 
-  // ITERATION 3 - Like Buttons
-
-  // Your code goes here ...
-
-  
-  
 
 
 // Bonus: ITERATION 4 - Add Random Contacts
