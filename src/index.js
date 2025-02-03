3a9a7e6af8fff12f106070927e35684d87edcd51
mbCeli <munbaporicelina@outlook.com>
2025-02-03 13:03:14 +0000

    done without bonus

--- src/index.js
+++ src/index.js
@@ -39,20 +39,41 @@ tableBody.appendChild(exampleRow);
 const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
+  threeContacts.forEach(contact => {
+    const otherRows = document.createElement("tr");
+    otherRows.innerHTML = `
+  <td>
+    <img src="${contact.pictureUrl}" />
+  </td>
+  <td> ${contact.name} </td>
+  <td> ${contact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
 
+    tableBody.appendChild(otherRows);
 
-  
-  // ITERATION 2 - Delete Buttons
-  
-  // Your code goes here ...
-  
-  
+    // ITERATION 2 - Delete Buttons
 
-  // ITERATION 3 - Like Buttons
+    // Your code goes here ...
+    const deleteButtons = otherRows.querySelector(".btn-delete");
 
-  // Your code goes here ...
+    deleteButtons.onclick = () => otherRows.remove();
 
-  
+    // ITERATION 3 - Like Buttons
+
+    // Your code goes here ...
+        const likeButtons = otherRows.querySelector(".btn-like");
+
+        likeButtons.onclick = () => likeButtons.classList.toggle("selected");
+  })
+
   
 
 
