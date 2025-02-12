9fc1c3df12c95c6b659d65c0dd762a24ab849533
Samuel Perez Besada <samusitesi@gmail.com>
2025-02-12 15:31:59 +0100

    done

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@
 const buttonAddRandom = document.querySelector("#btn-add-random");
 const tableBody = document.querySelector("tbody#contacts");
 
-
-// ITERATION 0 | Example Row
+/*// ITERATION 0 | Example Row
 // Splice 1 element from the contacts array at the random index
 const randomIndex = Math.floor(Math.random() * contacts.length);
 const splicedArr = contacts.splice(randomIndex, 1);
@@ -28,34 +27,90 @@ exampleRow.innerHTML = `
   </td>
 `;
 
-tableBody.appendChild(exampleRow);
-
-
-
-
+tableBody.appendChild(exampleRow);*/
 
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
 
 // Your code goes here ...
+threeContacts.forEach((actualContact) => {
+  let row = document.createElement("tr");
+  row.innerHTML = `
+      <td>
+        <img src="${actualContact.pictureUrl}" />
+      </td>
+      <td> ${actualContact.name} </td>
+      <td> ${actualContact.popularity.toFixed(2)} </td>
+      <td>
+        <button class="btn-delete">Delete</button>
+      </td>
+      <td>
+        <button class="btn-like">
+          <img src="./images/icon.png" alt="like" />
+        </button>
+      </td>
+    `;
 
+  tableBody.appendChild(row);
 
-  
   // ITERATION 2 - Delete Buttons
-  
+
   // Your code goes here ...
-  
-  
+  let deleteButton = row.querySelector(".btn-delete");
+
+  deleteButton.addEventListener("click", () => {
+    console.log("Clickando boton");
+    row.remove();
+  });
 
   // ITERATION 3 - Like Buttons
 
   // Your code goes here ...
-
-  
-  
-
+  let likeButton = row.querySelector(".btn-like");
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("selected");
+  }); /*FUNCIONA PERO PREGUNTAR JORGE*/
+});
 
 // Bonus: ITERATION 4 - Add Random Contacts
 
 // Your code goes here ...
+buttonAddRandom.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * contacts.length);
+  const splicedArr = contacts.splice(randomIndex, 1);
+
+  // Get the element from the spliced array
+  const randomContact = splicedArr[0];
+
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>
+      <img src="${randomContact.pictureUrl}" />
+    </td>
+    <td> ${randomContact.name} </td>
+    <td> ${randomContact.popularity.toFixed(2)} </td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+  `;
+
+  tableBody.appendChild(row);
+
+  let deleteButton = row.querySelector(".btn-delete");
+
+  deleteButton.addEventListener("click", () => {
+    console.log("Clickando boton");
+    row.remove();
+  });
+
+  let likeButton = row.querySelector(".btn-like");
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("selected");
+  }); /*FUNCIONA PERO PREGUNTAR JORGE*/
+});
